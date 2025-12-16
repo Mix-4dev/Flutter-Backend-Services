b1f811f4593a3fcbdc7e7c5f1aad05e063d5d8d8
Mix-4dev <dariusmix880@gmail.com>
2025-12-16 15:49:09 +0200

    DB configration settings
    Setting DB connection string along with DB password and user access and
    roles. Using a DBaaS for handling future problems like scalability and
    so on

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((conn) => {
+    // console.log(conn.connections);
+    console.log('DB connection successfully');
+  })
+  .catch((err) => console.log(err.message));
 
 // console.log(process.env);
 const PORT = process.env.PORT || 3000;
