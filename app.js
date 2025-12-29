6c0ecf6855317d7bb9c6a836ef22e74d0c973d07
Mix-4dev <dariusmix880@gmail.com>
2025-12-29 17:33:59 +0200

    user resource setup
    Mounting the user resource to the main express app + adding empty
    userController file + empty userModel file and finally the boilerplate
    code for userRoutes.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const itemRouter = require('./routes/itemRoutes');
+const userRouter = require('./routes/userRoutes');
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -13,4 +14,5 @@ if (process.env.NODE_ENV === 'development') {
   app.use('/', morgan('dev'));
 }
 app.use('/api/v1/items', itemRouter);
+app.use('/api/v1/users', userRouter);
 module.exports = app;
